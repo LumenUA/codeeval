e41ca874f15f71d485767a5120fb794e8d5620dd
Igor <lumenwin@gmail.com>
2016-03-22 15:42:49 +0200

    Create N Mod M.php

---
+++ easy/N Mod M.php
@@ -0,0 +1,9 @@
+<?php
+$fh = fopen($argv[1], 'r');
+while (false !== ($line = fgets($fh))) {
+    list($n, $m) = explode(',', trim($line));
+    while ($n >= $m) {
+       $n -= $m;
+    }
+    echo $n . PHP_EOL;
+}
